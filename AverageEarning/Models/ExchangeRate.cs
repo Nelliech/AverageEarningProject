92d317ccca8d04f77f601ed66e8306c3d0aba681
Michał Kijewski <m.kijewski142@gmail.com>
2022-01-28 14:10:57 +0100

    Add model ExchangeRate witch propertis using "Paste Special"

---
+++ AverageEarning/Models/ExchangeRate.cs
@@ -0,0 +1,22 @@
+﻿namespace AverageEarning.Models
+{
+    public class ExchangeRate
+    {
+
+        public class Rootobject
+        {
+            public string table { get; set; }
+            public string currency { get; set; }
+            public string code { get; set; }
+            public Rate[] rates { get; set; }
+        }
+
+        public class Rate
+        {
+            public string no { get; set; }
+            public string effectiveDate { get; set; }
+            public float mid { get; set; }
+        }
+
+    }
+}
