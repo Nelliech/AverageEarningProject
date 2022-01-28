147b12026985fad342e44462da0cfa6ec46b9cdc
Michał Kijewski <m.kijewski142@gmail.com>
2022-01-28 14:58:26 +0100

    Add class Country with property

---
+++ AverageEarning/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace AverageEarning.Models
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public float Tax { get; set; }
+        public float FixedCost { get; set; }
+    }
+}
