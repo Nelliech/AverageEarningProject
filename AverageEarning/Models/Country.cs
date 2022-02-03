779779219c0ef0b5e05231f94d34fd37ea8b3576
Michał Kijewski <m.kijewski142@gmail.com>
2022-02-03 18:30:26 +0100

    Remove "?" from property "TAX"

--- AverageEarning/Models/Country.cs
+++ AverageEarning/Models/Country.cs
@@ -4,7 +4,7 @@
     {
         public string? Name { get; set; }
         public string? Code { get; set; }
-        public float? Tax { get; set; }
+        public float Tax { get; set; }
         public float? FixedCost { get; set; }
     }
 }
