d99effd36f119aade6a58c1ab7579bdcf67d5d5b
Michał Kijewski <m.kijewski142@gmail.com>
2022-02-03 14:27:01 +0100

    Improvement Non-nullable property

--- AverageEarning/Models/ExchangeRate.cs
+++ AverageEarning/Models/ExchangeRate.cs
@@ -2,10 +2,10 @@
 {
     public class ExchangeRate
     {
-        public string Table { get; set; }
-        public string Currency { get; set; }
-        public string Code { get; set; }
-        public Rate[] Rates { get; set; }
+        public string? Table { get; set; }
+        public string? Currency { get; set; }
+        public string? Code { get; set; }
+        public Rate[]? Rates { get; set; }
     }
 
 
