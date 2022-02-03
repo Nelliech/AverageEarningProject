69b3d78c85792483436b3ac3152274c819ad7428
Michał Kijewski <m.kijewski142@gmail.com>
2022-02-03 14:27:30 +0100

    Improvement Non-nullable property

--- AverageEarning/Models/Country.cs
+++ AverageEarning/Models/Country.cs
@@ -2,9 +2,9 @@
 {
     public class Country
     {
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public float Tax { get; set; }
-        public float FixedCost { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+        public float? Tax { get; set; }
+        public float? FixedCost { get; set; }
     }
 }
