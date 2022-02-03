b5d196bdaab0134b304f136f2cd5e5f261f68b7b
Michał Kijewski <m.kijewski142@gmail.com>
2022-02-03 17:40:50 +0100

    Correct property

--- AverageEarning/Models/ExchangeRate.cs
+++ AverageEarning/Models/ExchangeRate.cs
@@ -1,12 +1,21 @@
 ﻿namespace AverageEarning.Models
 {
+    //public class ExchangeRate
+    //{
+    //    public string? Table { get; set; }
+    //    public string? Currency { get; set; }
+    //    public string? Code { get; set; }
+    //    public Rate[]? Rates { get; set; }
+    //}
+
+
     public class ExchangeRate
     {
-        public string? Table { get; set; }
-        public string? Currency { get; set; }
-        public string? Code { get; set; }
-        public Rate[]? Rates { get; set; }
+        public string Table { get; set; }
+        public string No { get; set; }
+        public string EffectiveDate { get; set; }
+        public Rate[] Rates { get; set; }
     }
-
-
 }
+
+
