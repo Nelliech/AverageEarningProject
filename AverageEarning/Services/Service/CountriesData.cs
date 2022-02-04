d0945a43d4e874dc85f4348183f5a376a90e252c
Michał Kijewski <m.kijewski142@gmail.com>
2022-02-04 14:55:30 +0100

    Change "Euro" to "EUR

--- AverageEarning/Services/Service/CountriesData.cs
+++ AverageEarning/Services/Service/CountriesData.cs
@@ -9,7 +9,7 @@ namespace AverageEarning.Services.Service
         {
             ICollection<Country> Countries = new List<Country>() {
             new Country() { Name="UK", Code = "GBP", Tax = 25, FixedCost = 600 },
-            new Country() { Name="DE", Code = "Euro", Tax = 20, FixedCost = 800 },
+            new Country() { Name="DE", Code = "EUR", Tax = 20, FixedCost = 800 },
             new Country() { Name="PL", Code = "PLN", Tax = 19, FixedCost = 1200 }
             };
 
